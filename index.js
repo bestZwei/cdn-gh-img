bb929bc8228a65c4b9397544f5ed30bf37c09839
developer <noreply@null.org>
2024-09-16 21:45:22 +0800

    更新

--- index.js
+++ index.js
@@ -1,16 +1,8 @@
 export default {
-  async fetch(request, env) {
+  async fetch(request) {
     const url = new URL(request.url);
     const path = url.pathname.substring(1); // 获取路径，去掉开头的斜杠
     const [owner, repo, ...filePath] = path.split('/');
-
-    // 从环境变量中获取允许的仓库列表
-    const allowedRepos = env.ALLOWED_REPOS.split(',');
-
-    // 检查请求的仓库是否在允许列表中
-    if (!allowedRepos.includes(`${owner}/${repo}`)) {
-      return new Response('Repository not allowed', { status: 403 });
-    }
 
     // GitHub 原始内容 URL
     const githubUrl = `https://raw.githubusercontent.com/${owner}/${repo}/master/${filePath.join('/')}`;
